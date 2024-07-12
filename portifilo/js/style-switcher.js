ebf5600bfc84a0f8e173bf86e30c12dd789fcdeb
mhussien1102 <143397704+mhussien1102@users.noreply.github.com>
2024-07-13 02:01:20 +0300

    Add files via upload

---
+++ portifilo/js/style-switcher.js
@@ -0,0 +1,12 @@
+/*start toogle */
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+
+
+const run = document.querySelector(".style-switcher");
+
+
+
+    
+    console.log(run);
+
+
